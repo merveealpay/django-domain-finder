ed6cac061796cb0f3283abcc1f71d0f6a72dc77d
merveealpay <merveealpay@gmail.com>
2021-05-02 17:24:58 +0300

    added domainview,url and basemodel

--- domain-finder/applications/finder/models.py
+++ domain-finder/applications/finder/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
+from applications.core.models import TimeStampBaseModel
 
-class Provider(models.Model):
+
+class Provider(TimeStampBaseModel):
     country = models.CharField(max_length=250)
     ip_address = models.GenericIPAddressField()
     detail = models.TextField(null=True)
-    created_at = models.DateTimeField(auto_created=True)
-    update_at = models.DateTimeField(auto_created=True)
 
 
-class Domain(models.Model):
+class Domain(TimeStampBaseModel):
     domain = models.CharField(max_length=100)
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_created=True)
-    update_at = models.DateTimeField(auto_created=True)
--- domain-finder/config/urls.py
+++ domain-finder/config/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('domain/', include(('applications.finder.urls', 'domain'), namespace='domain', ))
 ]
